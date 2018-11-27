868590defb6a230f42550a07d83aa9a6f5e5d1b9
Ian McDermott <imcd.po@gmail.com>
2018-11-26 22:17:42 -0500

    Start switch functionality, fix LED - no delay

---
+++ Charlieplex.ino
@@ -0,0 +1,54 @@
+
+
+/*
+   If you want to save bytes in SRAM, this could be a collection of bytes for each DDRx and PORTx
+   on the AVR chip (up to 8 bits or lines) (8 lines would allow for 56 LEDs)
+*/
+int matrix[LED_COUNT][2][PIN_COUNT] = {
+  /*
+      Each row in this matrix respresents the pin modes and pin states for a single LED
+  */
+  //              PIN_CONFIG                          PIN_STATE
+  //    A       B       C      D      E         A     B    C    D    E
+  { { INPUT, INPUT, OUTPUT, INPUT,  OUTPUT}, { LOW, LOW, HIGH, LOW,  LOW } }, // CD 4
+  { { INPUT, INPUT, OUTPUT, OUTPUT,  INPUT}, { LOW, LOW, HIGH, LOW, LOW } }, // AB 0
+  { { INPUT, INPUT, INPUT, OUTPUT,  OUTPUT}, { LOW, LOW, LOW, LOW, HIGH } }, // BC 2
+  { { INPUT, INPUT, OUTPUT, INPUT, OUTPUT},  { LOW, LOW, LOW, LOW , HIGH  } }, // DC 5
+
+  { { INPUT, INPUT, INPUT, OUTPUT,  OUTPUT},  { LOW, LOW, LOW, HIGH, LOW  } },  // BA 1
+  { { INPUT, INPUT, OUTPUT, OUTPUT, INPUT },  { LOW, LOW, LOW, HIGH, LOW } }, // CB 3
+  { { INPUT, OUTPUT, INPUT, INPUT,  OUTPUT }, { LOW, HIGH, LOW, LOW, LOW } }, // ED 6
+  { { INPUT, OUTPUT, OUTPUT, INPUT, INPUT  }, { LOW, HIGH, LOW, LOW, LOW } }, // ED 6
+
+  { { OUTPUT, INPUT, INPUT, INPUT,  OUTPUT }, { HIGH, LOW, LOW, LOW, LOW } }, // AC 8
+  { { OUTPUT, INPUT, OUTPUT, INPUT, INPUT }, { HIGH, LOW, LOW, LOW, LOW } }, // CA 9
+  { { OUTPUT, OUTPUT, INPUT, INPUT, INPUT }, { HIGH, LOW, LOW, LOW, LOW } }, // CA 9
+  { { OUTPUT, INPUT, INPUT, OUTPUT,  INPUT }, { HIGH, LOW, LOW, LOW, LOW } }, // AC 8
+
+  { { OUTPUT, OUTPUT, INPUT, INPUT, INPUT }, { LOW, HIGH, LOW, LOW, LOW } },  // DB 13
+  { { INPUT, OUTPUT, INPUT, OUTPUT, INPUT }, { LOW, HIGH, LOW, LOW, LOW } },  // DB 14
+  { { INPUT, OUTPUT, INPUT, OUTPUT, INPUT }, { LOW, LOW, LOW, HIGH, LOW } },  // DA 15
+  { { OUTPUT, INPUT, INPUT, OUTPUT, INPUT }, { LOW, LOW, LOW, HIGH, LOW } },  // BE 16
+
+  { { OUTPUT, INPUT, OUTPUT, INPUT, INPUT }, { LOW, LOW, HIGH, LOW, LOW } },  // EB 17
+  { { INPUT, OUTPUT, OUTPUT, INPUT, INPUT }, { LOW, LOW, HIGH, LOW, LOW } },  // AE 18
+  { {  INPUT, OUTPUT, INPUT, INPUT, OUTPUT }, { LOW, LOW, LOW, LOW, HIGH } }, // EA 19
+  { { OUTPUT, INPUT, INPUT, INPUT, OUTPUT }, { LOW, LOW, LOW, LOW, HIGH } } // EA 19
+
+};
+
+void turnOn( int led ) {
+  // set all the pin modes
+  pinMode( A, matrix[led][PIN_CONFIG][0] );
+  pinMode( B, matrix[led][PIN_CONFIG][1] );
+  pinMode( C, matrix[led][PIN_CONFIG][2] );
+  pinMode( D, matrix[led][PIN_CONFIG][3] );
+  pinMode( E, matrix[led][PIN_CONFIG][4] );
+  // set all the pin states
+  digitalWrite( A, matrix[led][PIN_STATE][0] );
+  digitalWrite( B, matrix[led][PIN_STATE][1] );
+  digitalWrite( C, matrix[led][PIN_STATE][2] );
+  digitalWrite( D, matrix[led][PIN_STATE][3] );
+  digitalWrite( E, matrix[led][PIN_STATE][4] );
+}
+
--- Hemmer-O-Graph.ino
+++ Hemmer-O-Graph.ino
@@ -1,31 +1,22 @@
-/*void setup() {
-  // put your setup code here, to run once:
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  }
+// Original Code by Andy
+//https://www.stuffandymakes.com/blog/2015/4/28/charlieplexing-more-leds-20-to-be-exact
 
-  void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(11, HIGH);
-  delay(500);
-  digitalWrite(10, HIGH);
-  delay(250);
-    digitalWrite(11, LOW);
-  delay(250);
-  digitalWrite(10, LOW);
+// Made for common-anode tri-color LEDs
 
-  }
-*/
+// pin defines
+#define A 5
+#define B 6
+#define C 9
+#define D 10
+#define E 11
 
-// Shows how to run three Steppers at once with varying speeds
-//
-// Requires the Adafruit_Motorshield v2 library
-//   https://github.com/adafruit/Adafruit_Motor_Shield_V2_Library
-// And AccelStepper with AFMotor support
-//   https://github.com/adafruit/AccelStepper
+#define PIN_COUNT 5
 
-// This tutorial is for Adafruit Motorshield v2 only!
-// Will not work with v1 shields
+#define PIN_CONFIG 0
+#define PIN_STATE 1
+
+#define LED_COUNT 20
+
 
 #include <Wire.h>
 #include <AccelStepper.h>
@@ -49,31 +40,34 @@ Adafruit_StepperMotor *myStepper2 = AFMStop.getStepper(200, 2);
 const int OUTPUT_TYPE = SERIAL_PLOTTER;
 
 const int PULSE_INPUT = 0;
-const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
-const int PULSE_FADE = 5;
 const int THRESHOLD = 550;   // Adjust this number to avoid noise when idle
 int rotateDelay = 25;
 float pulseVal;
 
 const int MODE_SWITCH = 2;
-const int PERSON_SWITCH = 3;
+const int PERSON_SWITCH_1 = 3;
+const int PERSON_SWITCH_1_INPUT = 4;
+const int PERSON_SWITCH_2 = 7;
+const int PERSON_SWITCH_2_INPUT = 8;
 
-bool pulseMode = true;
-bool drawingMode = false;
+bool pulseMode;
+bool drawingMode;
 bool bpmIndex = false;
-
+bool previousDrawingMode1 = false;
+bool previousDrawingMode2 = false;
+bool previousPulseMode1 = false;
+bool     previousMode = false;
 long newBpm0, newBpm1;
 // LED INDICATORS ///
 
-const int RED4 = 13;          // The on-board Arduino LED, close to PIN 13.
-const int RED3 = 11;
-const int RED2 = 9;
-const int RED1 = 7;
 
-const int GREEN4 = 12;          // The on-board Arduino LED, close to PIN 13.
-const int GREEN3 = 10;
-const int GREEN2 = 8;
-const int GREEN1 = 6;
+int pulseModeCount = 0;
+
+const int PWM1 = 5;
+const int PWM2 = 6;
+const int PWM3 = 9;
+const int PWM4 = 10;
+const int PWM5 = 11;
 
 // you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
 // wrappers for the first motor!
@@ -100,15 +94,17 @@ int bpm1 ;
 AccelStepper stepper1(forwardstep1, backwardstep1);
 AccelStepper stepper2(forwardstep2, backwardstep2);
 
+// variables to allow leds to blink without delays
+unsigned long previousMillis = 0;
+const long interval = 100;
+int blinkCount = 0;
+
 void setup()
 {
   Serial.begin(115200);
   // Configure the PulseSensor manager.
   pulseSensor.analogInput(PULSE_INPUT);
-  //  pulseSensor.blinkOnPulse(PULSE_BLINK);
-  //  pulseSensor.fadeOnPulse(PULSE_FADE);
 
-  //  pulseSensor.setSerial(Serial);
   //  pulseSensor.setOutputType(OUTPUT_TYPE);
   pulseSensor.setThreshold(THRESHOLD);
 
@@ -122,54 +118,63 @@ void setup()
        If your Sketch hangs here, try changing USE_ARDUINO_INTERRUPTS to false.
        which doesn't use interrupts.
     */
-
-    pinMode(MODE_SWITCH, INPUT);
-
     for (;;) {
-      // Flash the led to show things didn't work.
-      digitalWrite(PULSE_BLINK, LOW);
-      delay(50);
-      digitalWrite(PULSE_BLINK, HIGH);
-      delay(50);
+      //      // Flash the led to show things didn't work.
+      //      digitalWrite(PULSE_BLINK, LOW);
+      //      delay(50);
+      //      digitalWrite(PULSE_BLINK, HIGH);
+      //      delay(50);
     }
-  }
+    checkMode();
 
-//  AFMSbot.begin(); // Start the bottom shield
+  }
+  pinMode(MODE_SWITCH, INPUT);
+  pinMode(PERSON_SWITCH_1, OUTPUT);
+  pinMode(PERSON_SWITCH_1_INPUT, INPUT);
+  pinMode(PERSON_SWITCH_2, OUTPUT);
+  pinMode(PERSON_SWITCH_2_INPUT, INPUT);
+
+  //  AFMSbot.begin(); // Start the bottom shield
   AFMStop.begin(); // Start the top shield
-  int bpm0 = 0;
-  int bpm1 = 0;
+  int bpm0 = 240;
+  int bpm1 = 120;
   //get least common multiple of bpms to calculate moveto
+  newBpm0 = bpm0 * 200L * 60L;
+  newBpm1 = bpm1 * 200L * 60L;
   lcm = getLCM(bpm0, bpm1);
 
-  bpm0 *= 200 * 60;
+  Serial.print("BPM 0: "); Serial.println(newBpm0);
+  Serial.print("BPM 1: "); Serial.println(newBpm1);
 
-  bpm1 *= 200 * 60;
-  stepper1.setMaxSpeed(600000);
-  stepper1.setAcceleration(60);
-  stepper1.moveTo(100000);
+  stepper1.setMaxSpeed(newBpm0);
+  stepper1.setAcceleration(newBpm0);
+  stepper1.moveTo(50000);
   delay(5);
   //set max speed to bpm since speed is set to rpm
   //  stepper2.setMaxSpeed(bpm1);
   //  stepper2.setAcceleration(20);
-  stepper2.moveTo(lcm * 200 / 6);
+
+  stepper2.setMaxSpeed(newBpm1);
+  stepper2.setAcceleration(newBpm1);
+  stepper2.moveTo(50000 );
   //  updateRPM();
-  newBpm0 = 1176000;
 
-  // LED INDICATORS ///
-  pinMode(RED4, OUTPUT);
-  pinMode(GREEN3, OUTPUT);
-  pinMode(RED2, OUTPUT);
-  pinMode(RED1, OUTPUT);
-
-  pinMode(GREEN4, OUTPUT);
-  pinMode(GREEN3, OUTPUT);
-  pinMode(GREEN2, OUTPUT);
-  pinMode(GREEN1, OUTPUT);
+  //    stepper1.setMaxSpeed(100.0);
+  //  stepper1.setAcceleration(100.0);
+  //  stepper1.moveTo(24);
+  //
+  //  stepper2.setMaxSpeed(200.0);
+  //  stepper2.setAcceleration(100.0);
+  //  stepper2.moveTo(50000);
 
 }
 
 void loop()
 {
+  if (pulseMode) {
+    Serial.println("Pulse Mode Activated");
+  }  else {
+  }
 
   //  digitalWrite(PULSE_BLINK, LOW);
   //  delay(50);
@@ -186,33 +191,51 @@ void loop()
   //  Serial.println( newBpm0);
   //  bpm1 *= 200 * 60;
   //  // "myBPM" hold this BPM value now.
-//  stepper1.setSpeed(3000 );
+  //  stepper1.setSpeed(3000 );
+  unsigned long currentMillis = millis();
 
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    blinkCount++;
+    if(blinkCount > LED_COUNT){
+      blinkCount = 0;
+    }
+  }
+//  for ( int l = 0; l < LED_COUNT; l++ ) {
 
+    turnOn(blinkCount);
+    //      delay( 1000 / LED_COUNT );
+//  }
+
+    digitalWrite(PERSON_SWITCH_1, HIGH);
+  
+    digitalWrite(PERSON_SWITCH_2, HIGH);
 
   //  delay(20);
-  pulseSensor.outputSample();
-  pulseVal = pulseSensor.getLatestSample();
-  // Reset the current position to 0 to start a new drawing
-  // Check if we're in Pulse Mode or Drawing Mode
-  checkMode();
-  checkPerson();
-  if (drawingMode) {
-    drawImage();
-  }
-
-  if (pulseMode) {
-    if (!bpmIndex) {
-      bpm0 = int(pulseSensor.getBeatsPerMinute());  // Calls function on our pulseSensor object that returns BPM as an "int".
-    } else {
-      bpm1 = int(pulseSensor.getBeatsPerMinute());  // Calls function on our pulseSensor object that returns BPM as an "int".
+    pulseSensor.outputSample();
+    pulseVal = pulseSensor.getLatestSample();
+    // Reset the current position to 0 to start a new drawing
+    // Check if we're in Pulse Mode or Drawing Mode
+    checkMode();
+    checkPerson();
+    if (drawingMode) {
+  Serial.println("Draw Mode Activated");
+  drawImage();
     }
-    newBpm0 = bpm0 * 12000L;
 
-    updateRPM();
-    rotatePulse();
-    displayBPM(123);
-  }
+    if (pulseMode) {
+      if (!bpmIndex) {
+        bpm0 = int(pulseSensor.getBeatsPerMinute());  // Calls function on our pulseSensor object that returns BPM as an "int".
+      } else {
+        bpm1 = int(pulseSensor.getBeatsPerMinute());  // Calls function on our pulseSensor object that returns BPM as an "int".
+      }
+      newBpm0 = bpm0 * 12000L *2;
+  
+//      updateRPM();
+      //    rotatePulse();
+      //    displayBPM(123);
+    }
 }
 
 void drawImage() {
@@ -233,13 +256,13 @@ void drawImage() {
 
 void updateRPM() {
 
-  stepper1.setSpeed(newBpm0 );
-  stepper1.setAcceleration(newBpm0 / 10);
+  stepper1.setSpeed(newBpm0);
+  stepper1.setAcceleration(100);
   stepper1.moveTo(lcm * 200 / 6);
   delay(5);
   //set max speed to bpm since speed is set to rpm
-  stepper2.setSpeed(bpm1 * 20 );
-  stepper2.setAcceleration(20);
+  stepper2.setSpeed(newBpm1);
+  stepper2.setAcceleration(100);
   stepper2.moveTo(lcm * 200 / 6);
 
   delay(5);
@@ -262,158 +285,23 @@ int getLCM(int n1, int n2)
   return lcm;
 }
 
-/*
-  displays BPM using LEDs
-  Red = 100's
-  Green = 10's
-  Yellow = 1's
-
-            [Hemmer-O-Graph]
-  ________________________________________
-  |                                      |
-  |      • 0                  • 1        |
-  |                                      |
-  | • 2   ♥    • 3       • 4   ♥    • 5  |
-  |                                      |
-  |      • 6   • 7       • 8   • 9       |
-  |                                      |
-  ________________________________________
-*/
-void displayBPM(int currentBPM) {
-  String BPMString = String(currentBPM);
-  if (BPMString.length() == 3) {
-    int currentNum = int(BPMString.charAt(0));
-    display100s(currentNum, 3);
-  } else if (BPMString.length() == 2) {
 
 
-
-  } else if (BPMString.length() == 1) {
-
-  }
-}
-
-void  checkMode() {
-  //if the input is receiving voltage, mark as pulse mode
-  if (digitalRead(MODE_SWITCH) == 0) {
-    pulseMode = true;
-    drawingMode = false;
-    Serial.println("Pulse Mode Activated");
-  } else {
-    pulseMode = false;
-    drawingMode = true;
-    Serial.println("Drawing Mode Activated");
-  }
-}
 
 void  checkPerson() {
   //if the input is receiving voltage, mark as pulse mode
-  if (digitalRead(PERSON_SWITCH) == 0) {
+  if (digitalRead(PERSON_SWITCH_1) == 0) {
     bpmIndex = false;
-    Serial.println("Person 1");
+    //    Serial.println("Person 1");
   } else {
     bpmIndex = true;
-    Serial.println("Person 2");
+    //    Serial.println("Person 2");
   }
 }
 
 
-void display100s(int ledNumber, int decimalPlace) {
-  for (int i = 0; i < 10; i++) {
-    if (i = ledNumber) {
-      for (int i = 0; i < 5; i++) {
-        if (decimalPlace = 3) {
-          if (i % 2 == 0) {
-            digitalWrite(GREEN4, HIGH);
-            delay(50);
-            digitalWrite(GREEN4, LOW);
-            delay(50);
-          } else {
-            digitalWrite(ledNumber, HIGH);
-            delay(50);
-            digitalWrite(ledNumber, LOW);
-            delay(50);
-          }
-        }
-      }
-    } else {
-      if (i % 2 == 0) {
-        digitalWrite(i, HIGH);
-        delay(50);
-        digitalWrite(i, LOW);
-        delay(50);
-      } else {
-        digitalWrite(i, HIGH);
-        delay(50);
-        digitalWrite(i, LOW);
-        delay(50);
-      }
-    }
-  }
+//drive shaft
+//stepper mount
+//shaft coupler
+//mounting hub?
 
-  delay(5000);
-}
-
-
-
-void displayPulse(bool bpm) {
-}
-
-void rotatePulse() {
-  //  delay(20);
-  if (pulseSensor.isInsideBeat()) {
-    digitalWrite(GREEN4, HIGH);
-    delay(rotateDelay);
-    digitalWrite(RED4, HIGH);
-    digitalWrite(GREEN3, HIGH);
-    delay(rotateDelay);
-    digitalWrite(RED3, HIGH);
-    digitalWrite(GREEN4, LOW);
-    delay(rotateDelay);
-    digitalWrite(GREEN2, HIGH);
-    digitalWrite(RED4, LOW);
-    delay(rotateDelay);
-    digitalWrite(RED2, HIGH);
-    digitalWrite(GREEN3, LOW);
-    delay(rotateDelay);
-    digitalWrite(RED3, LOW);
-    digitalWrite(GREEN1, HIGH);
-    delay(rotateDelay);
-    digitalWrite(RED1, HIGH);
-    digitalWrite(GREEN2, LOW);
-    delay(rotateDelay);
-    digitalWrite(RED2, LOW);
-    digitalWrite(GREEN1, LOW);
-    delay(rotateDelay);
-    digitalWrite(RED1, LOW);
-    delay(rotateDelay);
-  }
-}
-
-/*    digitalWrite(GREEN4, HIGH);
-    delay(25);
-    digitalWrite(LED12, HIGH);
-    digitalWrite(GREEN3, HIGH);
-    delay(25);
-    digitalWrite(GREEN4, LOW);
-    delay(25);
-    digitalWrite(LED12, LOW);
-    digitalWrite(RED3,HIGH);
-    digitalWrite(GREEN2, HIGH);
-    delay(25);
-    digitalWrite(GREEN3, LOW);
-    delay(25);
-    digitalWrite(RED3, LOW);
-    digitalWrite(RED2, HIGH);
-    digitalWrite(GREEN1, HIGH);
-    delay(25);
-    digitalWrite(GREEN2, LOW);
-    delay(25);
-    digitalWrite(RED2, HIGH);
-    digitalWrite(GREEN1, LOW);
-    delay(25);
-
-    digitalWrite(RED1, HIGH);
-    digitalWrite(RED2, LOW);
-    delay(25);
-    digitalWrite(RED1, LOW);*/
